c1cb783921e8f9d60a5e33d17a304dd8e2d88ba3
Pierre-Jean Baraud <pierre-jean@baraud.fr>
2024-11-16 18:02:13 +0000

    rename cli and desktop to cli-base and desktop-base

--- nix/outputs.nix
+++ nix/outputs.nix
@@ -7,7 +7,7 @@ let
 in
 {
   packages.default = pkgs.cowsay;
-  packages.cli = import ./cli-base.nix { inherit pkgs; };
-  packages.desktop = import ./desktop-base.nix { inherit pkgs; };
+  packages.cli-base = import ./cli-base.nix { inherit pkgs; };
+  packages.desktop-base = import ./desktop-base.nix { inherit pkgs; };
   # nixosConfigurations."${system}" = pkgs.lib.attrsets.attrByPath (machine: { "${machine}" = import ./nixosConfiguration.nix; });
  }
